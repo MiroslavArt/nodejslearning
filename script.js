d268f17306c6f73a5ebd9ec2095872d7e1af18c8
Miros <lya-job@yandex.ru>
2021-11-09 19:26:10 +0300

    second commit

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 var a = process.argv[2]
 var b = process.argv[3]
 
-console.log(a)
-console.log(b)
+var arr = [2,3,5]
+
+if(/^(0|[1-9]\d*)$/.test(a) && /^(0|[1-9]\d*)$/.test(b)) {
+    arr.forEach(function(item, i, arr) {        
+        if(item>=a && item<=b) {
+            console.log(item)           
+        } else {
+            console.log("Число "+item+" вне заданного диапозона")
+        }   
+    })
+} else {
+    console.log("Ошибка в задании диапазона")    
+}
 
