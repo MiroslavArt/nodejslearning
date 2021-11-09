c3a948bd7ce7fdc1b10dfc6c0e1d2563e7e0bd45
Miros <lya-job@yandex.ru>
2021-11-09 19:54:39 +0300

    lesson-result

--- script.js
+++ script.js
@@ -3,15 +3,23 @@ var b = process.argv[3]
 
 var arr = [2,3,5]
 
+const colors = require('colors');
+
 if(/^(0|[1-9]\d*)$/.test(a) && /^(0|[1-9]\d*)$/.test(b)) {
     arr.forEach(function(item, i, arr) {        
         if(item>=a && item<=b) {
-            console.log(item)           
+            if(i==0) {
+                console.log(colors.green(item)) 
+            } else if(i==1) {
+                console.log(colors.yellow(item))     
+            } else {
+                console.log(colors.red(item)) 
+            }                      
         } else {
-            console.log("Число "+item+" вне заданного диапозона")
+            console.log(colors.red("Число "+item+" вне заданного диапозона"))
         }   
     })
 } else {
-    console.log("Ошибка в задании диапазона")    
+    console.log(colors.red("Ошибка в задании диапазона"))    
 }
 
